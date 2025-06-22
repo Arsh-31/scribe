e959390ac83c44db3f706d0e6cecd45f09e515e0
Arsh <kaurarshpreet746@gmail.com>
2025-06-22 12:02:32 +0530

    fixed navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   if (!authenticated) {
     return (
       <div className="w-full flex justify-center items-center h-screen">
-        <p>You are not logged in. Redirecting to login...</p>
+        <p>Redirecting to login...</p>
       </div>
     );
   }
